b55768a5fbe737098a2a3e59c626622e51d3b8c7
hml <1124791554@qq.com>
2023-10-13 22:25:40 +0800

    1、compileSdk 升级到 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.rocketreserver"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rocketreserver"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
